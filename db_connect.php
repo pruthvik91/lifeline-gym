447fbd8ef1771cfc5e598757d268b062af3c3b4b
pruthvik_ifox <pruthvikdhamecha@ifoxsolutions.com>
2026-05-11 18:28:04 +0530

    chore: update database connection credentials and schema name

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php 
 
-$conn= new mysqli('localhost','root','root','gym_db')or die("Could not connect to mysql".mysqli_error($con));
-$pdoconn = new PDO("mysql:host=localhost;dbname=gym_db", "root", "root");
+$conn= new mysqli('localhost','u593981822_lifeline','B7w~;;>h$D6','u593981822_gym_db')or die("Could not connect to mysql".mysqli_error($con));
+$pdoconn = new PDO("mysql:host=localhost;dbname=u593981822_gym_db", "u593981822_lifeline", 'B7w~;;>h$D6');
 $pdoconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
